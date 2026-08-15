0eb01b4bb1fc29efe44de013bed199fdf35c6ecd
Rafael  Rotela <rjvrafael22@gmail.com>
2026-08-14 23:53:01 -0400

    study(udemy): pratica operador ternario

---
+++ udemy/operadorTernario/script.js
@@ -0,0 +1,6 @@
+// (condicao) 'Valor para verdadeiro':'Valor para falso';
+
+const pontuacao = 5100;
+const nivelUsuario = pontuacao >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+const corUsuario = null
+console.log(nivelUsuario);
