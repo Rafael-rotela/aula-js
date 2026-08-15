7a4da73c143fcda4964b33cfe4d29881198570a5
Rafael  Rotela <rjvrafael22@gmail.com>
2026-08-14 23:53:00 -0400

    study(udemy): pratica desestruturacao de objetos

---
+++ udemy/destruction/script.js
@@ -0,0 +1,33 @@
+// ... rest, ... spread
+/*
+const numeros = [ 10000, 22222, 30000, 40000, 5000, 6000, 7000, 8000];
+const [um, , tres, , cinco, , sete] = numeros;
+console.log(um,tres,cinco);
+*/
+
+//                    0      1       2    
+//                 0 1 2   0 1 2   0 1 2
+// const numeros = [[1,2,3],[4,5,6],[7,8,9]];
+// const [, [, ,seis]] = numeros;
+// console.log(seis);
+
+// Atribuição via desestruturação(Objetos)
+
+const pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'Rotela',
+    idade: 20,
+    endereco: {
+        rua: 'Av Brasil',
+        numero: 203
+    }
+}
+const {endereco: {rua, numero}, endereco} = pessoa;
+console.log(rua,numero, endereco);
+
+
+
+// const { nome, sobrenome, idade } = pessoa;
+// console.log(nome, sobrenome);
+
+
